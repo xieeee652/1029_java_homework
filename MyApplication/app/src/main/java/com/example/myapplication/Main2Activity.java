6a62837072adcda17c03074ee5a320aeb7a46337
xieeee652 <peterxie0811@gmail.com>
2021-10-29 00:32:27 +0800

    homework

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button send_btn;
+    private EditText set_drink;
+    private RadioGroup rg1,rg2;
+
+    private String sugar = "無糖";
+    private String ice_opt = "去冰";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        rg1 = findViewById(R.id.radioGroup);
+        rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.radioButton1:
+                        sugar = "無糖";
+                        break;
+                    case R.id.radioButton2:
+                        sugar = "少糖";
+                        break;
+                    case R.id.radioButton3:
+                        sugar = "半糖";
+                        break;
+                    case R.id.radioButton4:
+                        sugar = "全糖";
+                        break;
+                }
+            }
+        });
+        rg2 = findViewById(R.id.radioGroup2);
+        rg2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i){
+                    case R.id.radioButton5:
+                        sugar = "微冰";
+                        break;
+                    case R.id.radioButton6:
+                        sugar = "少冰";
+                        break;
+                    case R.id.radioButton7:
+                        sugar = "正常冰";
+                        break;
+
+                }
+            }
+        });
+        send_btn =findViewById(R.id.btn_send);
+        send_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                set_drink = findViewById(R.id.ed_drink);
+                String drink = set_drink.getText().toString();
+                Intent i = new Intent();
+                Bundle b = new Bundle();
+                b.putString("sugar",sugar);
+                b.putString("drink",drink);
+                b.putString("ice",ice_opt);
+                i.putExtras(b);
+                setResult(101,i);
+                finish();
+            }
+        });
+        /*
+        Bundle bundle = getIntent().getExtras().getInt("key");
+//        int getData = getIntent().getExtras().getInt("key");
+        int value1 = bundle.getInt("value1");
+        String value2 = bundle.getString("value2");
+        int resultCode = 0;
+        Intent intent = new intent();
+        bundle.putString("key", "value");
+        intent.putExtras(bundle);
+        setResult(resultCode,intent);
+        finish();
+
+         */
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private TextView tv_meal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_meal = findViewById(R.id.tv_meal);
+        btn = findViewById(R.id.btn_choice);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, Main2Activity.class), 1);
+            }
+        });
+    }
+ /*
+        int value1 = 123;
+        String value2 = "123";
+
+        Bundle bundle = new Bundle();
+        bundle.putInt("value1",value1);
+        bundle.putString("value2",value2);
+
+        Intent intent = new Intent(this,Main2Activity.class);
+        int requestCode = 100;
+        startActivityForResult(intent, requestCode);
+        */
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null) return;
+
+        if (requestCode == 1) {
+            if (requestCode == 101) {
+                Bundle b = data.getExtras();
+                String str1 = b.getString("drink");
+                String str2 = b.getString("sugar");
+                String str3 = b.getString("ice");
+                tv_meal.setText(String.format("飲料: %s\n\n甜度: %s\n\n冰塊: %s\n\n"+1, str1, str2, str3));
+            }
+             /*   case 100:
+                    if(resultCode == 0)
+                    {
+                        Bundle bundle = data.getExtras().getBundle("key");
+                        int value1 = bundle.getInt("value1");
+                        String value2 = bundle.getString("value2");
+                    }else(
+                            //Do Something
+                            )
+                            break;
+                default:
+                    break;
+            }
+        }
+        intent.putExtra("key",bundle);
+        startActivities(intent);
+
+
+              */
+        }
+    }
+}
